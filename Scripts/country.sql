86eca17580c1cb7fdc8acf5b4d4cc735dfdb03a6
Esdras Salazar <ezracode@gmail.com>
2016-04-24 22:05:41 -0600

    Copa America 1921
    Copa America 1921

--- Scripts/country.sql
+++ Scripts/country.sql
@@ -2,3 +2,4 @@ insert into country (code, name) values (54, 'Argentina');
 insert into country (code, name) values (55, 'Brazil');
 insert into country (code, name) values (56, 'Chile');
 insert into country (code, name) values (598, 'Uruguay');
+insert into country (code, name) values (595, 'Paraguay');
--- Scripts/torunament.sql
+++ Scripts/torunament.sql
@@ -2,5 +2,7 @@ insert into tournament (code, country) values
                        (1916, 54),
 					   (1917, 598),
 					   (1919, 55),
-					   (1920, 56);
+					   (1920, 56),
+					   (1921, 54);
+					   
 					   
