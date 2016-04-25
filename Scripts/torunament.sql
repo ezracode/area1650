6d3dce37bfd348c54b6889462615bdd8c02b16ae
Esdras Salazar <ezracode@gmail.com>
2016-04-24 22:53:13 -0600

    Copa America 1922
    Copa America 1922

--- Scripts/torunament.sql
+++ Scripts/torunament.sql
@@ -3,6 +3,6 @@ insert into tournament (code, country) values
 					   (1917, 598),
 					   (1919, 55),
 					   (1920, 56),
-					   (1921, 54);
-					   
+					   (1921, 54),
+					   (1922, 55);
 					   
