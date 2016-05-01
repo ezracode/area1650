ce348ed37eb0e45cb727a62ac7115bc684c01158
Esdras Salazar <ezracode@gmail.com>
2016-04-30 23:54:55 -0600

    Verificacion de tablas de campeonatos
    Verificacion de tablas de campeonatos

--- Scripts/business-queries.sql
+++ Scripts/business-queries.sql
@@ -1,5 +1,6 @@
 select a.code, b.name from tournament a, country b where a.country = b.code;
 
+/*Tabla por campeonato desde 1916 hasta 1967*/
 select 
     a.name, 
 	 sum(b.points) points, 
@@ -12,7 +13,7 @@ select
 	 (sum(f.goals) - sum(g.goals)) diff
 from 
 country a inner join game_score b inner join game h
-    on a.code = b.squad and b.matchid = h.matchid and h.game_type in (2, 7) and year(h.matchdate) = 1923
+    on a.code = b.squad and b.matchid = h.matchid and h.game_type in (2) and year(h.matchdate) = 1923
 left join game_score c
     on c.points = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
 left join game_score d
@@ -27,6 +28,35 @@ where b.time_type in (2, 4, 6)
 group by a.code
 order by points desc, diff desc, goals desc, again desc
 
+/*Tabla General todos los tiempos*/
+select 
+    a.name, 
+	 sum(b.points) points, 
+	 count(b.squad) games, 
+	 count(c.squad) win, 
+	 count(d.squad) draw, 
+	 count(e.squad) loose,
+	 sum(f.goals) goals,
+	 sum(g.goals) again,
+	 (sum(f.goals) - sum(g.goals)) diff
+from 
+country a inner join game_score b inner join game h
+    on a.code = b.squad and b.matchid = h.matchid and h.game_type in (1, 2, 4, 6, 7)
+left join game_score c
+    on c.points = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
+left join game_score d
+    on d.points = 1 and b.matchid = d.matchid and b.time_type = d.time_type and b.squad = d.squad
+left join game_score e
+    on e.points = 0 and b.matchid = e.matchid and b.time_type = e.time_type and b.squad = e.squad
+left join game_score f
+    on b.matchid = f.matchid and b.time_type = f.time_type and b.squad = f.squad
+left join game_score g
+    on b.matchid = g.matchid and b.time_type = g.time_type and b.squad <> g.squad
+where b.time_type in (2, 4, 6) 
+group by a.code
+order by points desc, diff desc, goals desc, again desc
+
+/*Tabla por grupo desde 1975 actualidad*/
 select 
     i.group_code,
     a.name, 
@@ -55,3 +85,32 @@ left join game_score g
 where b.time_type in (2, 4, 6) 
 group by a.code
 order by group_code, points desc, diff desc, goals desc, again desc
+
+/*Tabla por campeonato desde 1975 actualidad*/
+select 
+    a.name, 
+	 sum(b.points) points, 
+	 count(b.squad) games,
+	 max(game_type) game_type, 
+	 count(c.squad) win, 
+	 count(d.squad) draw, 
+	 count(e.squad) loose,
+	 sum(f.goals) goals,
+	 sum(g.goals) again,
+	 (sum(f.goals) - sum(g.goals)) diff
+from 
+country a inner join game_score b inner join game h 
+    on a.code = b.squad and b.matchid = h.matchid and h.game_type in (1, 2, 4, 6, 7) and year(h.matchdate) = 1975
+left join game_score c
+    on c.points = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
+left join game_score d
+    on d.points = 1 and b.matchid = d.matchid and b.time_type = d.time_type and b.squad = d.squad
+left join game_score e
+    on e.points = 0 and b.matchid = e.matchid and b.time_type = e.time_type and b.squad = e.squad
+left join game_score f
+    on b.matchid = f.matchid and b.time_type = f.time_type and b.squad = f.squad
+left join game_score g
+    on b.matchid = g.matchid and b.time_type = g.time_type and b.squad <> g.squad
+where b.time_type in (2, 4, 6) 
+group by a.code
+order by game_type desc, points desc, diff desc, goals desc, again desc
