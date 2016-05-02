0fc5924dc678a8a5f0cbc847aa6c4a930b8ca215
Esdras Salazar <ezracode@gmail.com>
2016-05-01 21:24:56 -0600

    Copa America 1979
    Copa America 1979

--- Scripts/business-queries.sql
+++ Scripts/business-queries.sql
@@ -71,7 +71,7 @@ select
 from 
 country a inner join game_score b inner join game h inner join group_stage i
     on a.code = b.squad and b.matchid = h.matchid and h.game_type in (2) and year(h.matchdate) = 1975 
-	 and b.squad = i.squad and i.group_code in ('A', 'B', 'C')
+	 and b.squad = i.squad and i.group_code in ('A', 'B', 'C')  and i.tournament = 1975
 left join game_score c
     on c.points = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
 left join game_score d
