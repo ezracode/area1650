a3f81550651c048a19038ffc610c3f6225e9bdc5
Esdras Salazar <ezracode@gmail.com>
2016-05-15 01:44:34 -0600

    All times table reviewed
    All times table reviewed

--- Scripts/copaamerica/business-queries.sql
+++ Scripts/copaamerica/business-queries.sql
@@ -41,7 +41,7 @@ select
 	 (sum(g.goals) - sum(i.goals)) diff
 from 
 country a inner join game_score b inner join game h 
-    on a.code = b.squad and b.matchid = h.matchid and b.time_type in (2,4) and h.game_type in (1, 2, 3, 4, 5, 6, 7, 8)
+    on a.code = b.squad and b.matchid = h.matchid and b.time_type in (2,4) and h.game_type in (1, 2, 3, 4, 5, 6, 7, 8) and year(h.matchdate) < 2016
 left join game_score c
     on  b.time_type = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
 left join game_score d
