4f0089531d3c20c1aa3fbb268454e34f7ff5709c
Esdras Salazar <ezracode@gmail.com>
2016-05-15 22:25:17 -0600

    Eurocopa 1976
    Eurocopa 1976

--- Scripts/eurocopa/tournament.sql
+++ Scripts/eurocopa/tournament.sql
@@ -3,3 +3,4 @@ insert into tournament (code, country) values (1960, 33);
 insert into tournament (code, country) values (1964, 34);
 insert into tournament (code, country) values (1968, 39);
 insert into tournament (code, country) values (1972, 32);
+insert into tournament (code, country) values (1976, 38);
