f7b3c01851848bc7eaa3252d29e6d9391123ce12
Esdras Salazar <ezracode@gmail.com>
2016-05-22 01:19:22 -0600

    All Times Table Review
    All Times Table Review

--- Scripts/eurocopa/business-queries.sql
+++ Scripts/eurocopa/business-queries.sql
@@ -128,3 +128,40 @@ order by game_type desc, points desc, diff desc, goals desc, again desc
 select a.*, b.*, a.goals, b.goals, c.matchdate from game_score a inner join game_score b inner join game c 
 on a.matchid = b.matchid and a.time_type = b.time_type and a.time_type in (2,4,6) and a.matchid = c.matchid
 where a.squad = 55 and b.squad = 598
+
+/*Participacion por pais*/
+select 
+    a.name, 
+    year(h.matchdate) championship,
+	 sum(b.points) points, 
+	 count(c.squad) games,
+	 count(d.squad) win, 
+	 count(e.squad) draw,
+	 count(f.squad) loose,
+ 	 sum(g.goals) goals,
+	 sum(i.goals) again,
+	 (sum(g.goals) - sum(i.goals)) diff
+from 
+country a inner join game_score b inner join game h 
+    on a.code = b.squad and b.matchid = h.matchid and b.time_type in (2,3,4,6) and h.game_type in (1, 2, 3, 4, 5, 6, 7, 8) and h.matchdate < '2016-05-15'
+left join game_score c
+    on  b.time_type = 2 and b.matchid = c.matchid and b.time_type = c.time_type and b.squad = c.squad
+left join game_score d
+    on d.points >= 2 and b.matchid = d.matchid and b.time_type = d.time_type and b.squad = d.squad
+left join game_score e
+    on e.points = 1 and b.matchid = e.matchid and b.time_type = e.time_type and b.squad = e.squad
+left join game_score f
+    on f.points = 0 and f.time_type = (select max(time_type) from game_score where matchid = f.matchid) 
+	                 and b.matchid = f.matchid and b.time_type = f.time_type and b.squad = f.squad
+left join game_score g        
+    on g.time_type = (select max(time_type) from game_score where matchid = g.matchid and time_type in (2,3,4,6))  
+	                 and b.matchid = g.matchid and b.time_type = g.time_type and b.squad = g.squad 
+left join game_score i
+    on i.time_type = (select max(time_type) from game_score where matchid = i.matchid and time_type in (2,3,4,6))  
+	                 and b.matchid = i.matchid and b.time_type = i.time_type and b.squad <> i.squad 
+/*where b.squad in (49228, 49)*/
+/*where b.squad in (42, 420)*/
+/*where b.squad in (38, 38111)*/
+where b.squad in (7097, 37517, 7)
+group by a.code, year(h.matchdate)
+order by year(h.matchdate) asc, points desc, diff desc, goals desc, again desc
