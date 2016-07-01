8e2fd216fd434883f4cb541d3108169c87290f46
Esdras Salazar <ezracode@gmail.com>
2016-07-01 01:20:45 -0600

    tournaments, olympics and UEFAEURO quarter-finals
    tournaments, olympics and UEFAEURO quarter-finals

---
+++ Scripts/olympics/game_type.sql
@@ -0,0 +1,13 @@
+insert into game_type (id, name) values 
+(1, 'Qualifier'),
+(2, 'Group Stage'),
+(3, 'Quarter-Final'),
+(4, 'Semi-Final'),
+(5, 'Third Place'),
+(6, 'Final'),
+(7, 'Play-Off'),
+(8, 'Final Round'),
+(9, 'Round of 16'),
+(10,'Exhibition');
+(11,'Second Place');
+(12,'Play-Off Exhibition');
---
+++ Scripts/olympics/tournament.sql
@@ -0,0 +1,6 @@
+delete from tournament where code > 0;
+insert into tournament (code, country) values (1900, 33);
+insert into tournament (code, country) values (1904, 1);
+insert into tournament (code, country) values (1905, 30);
+insert into tournament (code, country) values (1908, 44);
+
