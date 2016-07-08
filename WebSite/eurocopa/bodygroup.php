a4a4d6df25d12dad190c2a6f98b99b1cdd739f9f
Esdras Salazar <ezracode@gmail.com>
2016-07-07 21:30:01 -0600

    Semifinal 2 Eurocopa2016
    Semifinal 2 Eurocopa2016

--- WebSite/eurocopa/bodygroup.php
+++ WebSite/eurocopa/bodygroup.php
@@ -48,6 +48,12 @@
 		$script = $script . knockout_stage($year, $stage);
 		$script = $script . '						</div>';
 
+		$script = $script . '						<div id="final">';
+		$script = $script . '							<p>Final</p>';
+		$stage = 6;
+		$script = $script . knockout_stage($year, $stage);
+		$script = $script . '						</div>';
+		
 		$script = $script . '					</div>';
 		$script = $script . '				</body>';
 		$script = $script . '    </html>';
