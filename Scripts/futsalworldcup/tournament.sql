f8e1530c228163f025470d447045242baf766c32
Esdras Salazar <ezracode@gmail.com>
2016-09-04 21:02:52 -0600

    Futsal World Cup Championships
    Futsal World Cup 1989, 1992, 1996, 2000, 2004 and 2008

--- Scripts/futsalworldcup/tournament.sql
+++ Scripts/futsalworldcup/tournament.sql
@@ -1,2 +1,7 @@
 delete from tournament where code > 0;
 insert into tournament (code, country) values (1989, 31);
+insert into tournament (code, country) values (1992, 852);
+insert into tournament (code, country) values (1996, 34);
+insert into tournament (code, country) values (2000, 502);
+insert into tournament (code, country) values (2004, 886);
+insert into tournament (code, country) values (2008, 55);
