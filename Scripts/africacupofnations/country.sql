b50d245f08a2dc40e774c76390daa488e1a48c83
Esdras Salazar <ezracode@gmail.com>
2016-12-11 03:26:02 -0600

    Africa Cup of Nations 2017 Schedule
    Africa Cup of Nations 2017 Schedule

--- Scripts/africacupofnations/country.sql
+++ Scripts/africacupofnations/country.sql
@@ -45,3 +45,4 @@ insert into country (code, name) values (240, 'Equatorial Guinea');
 insert into country (code, name) values (227, 'Niger');
 insert into country (code, name) values (267, 'Botswana');
 insert into country (code, name) values (238, 'Cape Verde');
+insert into country (code, name) values (245, 'Guinea-Bissau');
