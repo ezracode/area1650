2b6a5abae65f5eece6db6a3e4d39e8d9d9ca4378
Esdras Salazar <ezracode@gmail.com>
2017-02-02 21:17:00 -0600

    Gabon 2017 Semifinals
    Gabon 2017 Semifinals

--- WebSite/africacupofnations/bodygroup.php
+++ WebSite/africacupofnations/bodygroup.php
@@ -41,7 +41,7 @@
 		$stage = 4;
 		$script = $script . knockout_stage($year, $stage);
 		$script = $script . '						</div>';
-/*		
+		
 		$script = $script . '						<div id="third-place">';
 		$script = $script . '							<p>Third Place</p>';
 		$stage = 5;
@@ -53,7 +53,7 @@
 		$stage = 6;
 		$script = $script . knockout_stage($year, $stage);
 		$script = $script . '						</div>';
-*/				
+				
 		$script = $script . '					</div>';
 		$script = $script . '				</body>';
 		$script = $script . '    </html>';
