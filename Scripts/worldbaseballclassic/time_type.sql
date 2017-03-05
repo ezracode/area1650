52e510c6b0fe2e5be818a2f7476fa4f23db03e03
Esdras Salazar <esalazar@localhost.localdomain>
2017-03-05 00:03:05 -0600

    2006 World Baseball Classic Group A and structure

---
+++ Scripts/worldbaseballclassic/time_type.sql
@@ -0,0 +1,27 @@
+insert into time_type (id, name) values 
+(1, 'Inning #1'),
+(2, 'Inning #2'),
+(3, 'Inning #3'),
+(4, 'Inning #4'),
+(5, 'Inning #5'),
+(6, 'Inning #6'),
+(7, 'Inning #7'),
+(8, 'Inning #8'),
+(9, 'Inning #9'),
+(10, 'Inning #10'),
+(11, 'Inning #11'),
+(12, 'Inning #12'),
+(13, 'Inning #13'),
+(14, 'Inning #14'),
+(15, 'Inning #15'),
+(16, 'Inning #16'),
+(17, 'Inning #17'),
+(18, 'Inning #18'),
+(9000, 'Runs'),
+(9001, 'Hits'),
+(9002, 'Errors');
+
+
+
+
+
---
+++ Scripts/worldbaseballclassic/tournament.sql
@@ -0,0 +1,5 @@
+delete from tournament where code > 0;
+insert into tournament (code, country) values (2006, 1);
+insert into tournament (code, country) values (2009, 1);
+insert into tournament (code, country) values (2013, 1);
+insert into tournament (code, country) values (2017, 1);
