bbdc81d65e99fa5adc6bac9011b21d158ced1125
Esdras Salazar <ezracode@gmail.com>
2017-06-26 23:24:37 -0600

    Gold Cup 1963 results
    Gold Cup 1963 results

--- Spider/results.py
+++ Spider/results.py
@@ -12,19 +12,17 @@ htmlElem = html.document_fromstring(sourceCode) #make HTML element object
 aDict = {}
 
 root = etree.HTML(sourceCode)
-#root.findall(".//table")
 
 for b1 in root.iterfind(".//table"):
     #table
     print ("****")
-    #print (b1)
     for c1 in  b1:
         #td
-        #print (c1)
+        informacion = ""
         for d1 in  c1:
             #tr
-            #print (d1)
             for e1 in  d1:
                 #elemento
-                #print (e1.items())
-                print (e1.text)
+                if e1.text is not None:
+                    informacion = informacion + "|" + e1.text
+        print (informacion.encode('UTF-8') )        
